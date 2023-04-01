b96f09837495c77be739612888c3ccfc95958548
Emre Yıldız <emreyildiz183@gmail.com>
2023-04-01 21:41:11 +0300

    Initial commit

---
+++ README.txt
@@ -0,0 +1,19 @@
+Kod nasıl çalışıyor ve ne zaman çalıştırılmalı:
+    !!!Kod düzgün çalışması için yalnız perşembe çalıştırılmalıdır.!!!
+    Kodu çalıştırmadan önce lütfen kullanıcı adınızı ve şifrenizi giriniz.
+    Kod 2 nolu yemekhaneden yemek alacaktır ama 70.satırdaki kodu değiştirirseniz diğer yemekhanelere geçiş yapabilirsiniz.
+    Kodun çalışmasını otomatikleştirmek için https://www.pythonanywhere.com/ tarzı bir server'a kod yüklenebilir. Bu sitede kodu ne zaman ve saat kaçta çalıştırabileceğinizi seçebiliyorsunuz. !Günü perşembe yapmayı unutmayınız.!
+    Kod sadece 1 haftalık öğle ve akşam yemeklerinizi alacak şekilde tasarlanmıştır.Kodu biraz özelleştirerek bunu kolaylıkla değiştirebilirsiniz.
+    Google chrome sürümünüz SeleniumDrivers dosyasındaki chrome driverı çalıştırmaya yetmeyebilir.Chrome'unuzun sürümünün uyumlu olduğundan emin olun veya chrome driverını kendi sürümünüze göre yükleyebilirsiniz.
+    Chromunuzun sürümüne bakmak için chrome://version/ url'ye gidiniz.
+    Kendinize uygun driver sürümlerini yüklemek için buraya bakabilirsiniz : https://chromedriver.chromium.org/downloads
+    Kodda hazır yüklü olan SeleniumDriversdaki chrome driverın sürümü 111.0.5563.64
+
+Koddaki olası eksikler:
+    Araya bir tatil günü girerse veya kullanıcı kendi eliyle ara tarihteki bir yemeği önceden aldıysa sonraki haftadan da yemek alınacak.
+    Herhangi bir yoğunluk durumunda site çökerse veya sitenin işleyişi çok yavaşlarsa kod hata verebilir.
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,85 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from time import sleep
+import datetime
+
+KULLANICI_ADI = "Lütfen bu alana sadece öğrenci numaranızı giriniz"
+SIFRE = "Lütfen bu alana şifrenizi giriniz"
+
+OGLE_YEMEK = "https://sks2noluyemek.ege.edu.tr/YemekRezervasyon.aspx?o=O&Yemekhane=02"
+AKSAM_YEMEK = "https://sks2noluyemek.ege.edu.tr/YemekRezervasyon.aspx?o=A&Yemekhane=02"
+SEPET = "https://sks2noluyemek.ege.edu.tr/Sepetim.aspx"
+bugun = int(datetime.datetime.now().day)
+buay = int(datetime.datetime.now().month)
+aylar = {
+    "ocak": 1,
+    "şubat": 2,
+    "mart": 3,
+    "nisan": 4,
+    "mayıs": 5,
+    "haziran": 6,
+    "temmuz": 7,
+    "ağustos": 8,
+    "eylül": 9,
+    "ekim": 10,
+    "kasım": 11,
+    "aralık": 12
+}
+
+os.environ["PATH"] += "SeleniumDrivers/chromedriver.exe"
+driver = webdriver.Chrome()
+
+
+def yemek_sec():
+    yemek_tarih = driver.find_element(By.XPATH, '//*[@id="ctl00"]/div[4]/div[2]/table/tbody/tr[2]/td[2]').text.split(
+        " ")
+    yemek_tarih_gun = int(yemek_tarih[0])
+    yemek_tarih_ay = aylar[yemek_tarih[1].lower()]
+
+    click_sayisi = 0
+    if yemek_tarih_ay == buay:
+        alinacak_son_gun = bugun + 10  # kod her perşembe gece 12'den öncki herhangi bir saate çalışıcaktır
+        if alinacak_son_gun >= yemek_tarih_gun:
+            click_sayisi = alinacak_son_gun - yemek_tarih_gun + 1
+
+    clickler = driver.find_elements(By.CLASS_NAME, "form-control")
+    sayac = 0
+    for click in clickler:
+        click.click()
+        sayac += 1
+        if sayac == click_sayisi:
+            break
+    driver.find_element(By.ID, "ContentPlaceHolder1_btnSepetEkle").click()
+
+
+driver.get("https://kimlik.ege.edu.tr/")
+
+username = driver.find_element(By.ID, "username")
+username.send_keys(KULLANICI_ADI)
+
+password = driver.find_element(By.ID, "password")
+password.send_keys(SIFRE)
+password.send_keys(Keys.ENTER)
+
+yemekhane = driver.find_element(By.CSS_SELECTOR, "#yemekhanebtn > span")
+yemekhane.click()
+
+sleep(1)
+driver.find_element(By.LINK_TEXT, "2 No'lu Yemekhane").click()
+sleep(2)  # web sitesi authentication'ı kodun çalıştığı kadar hızlı yapamadığı için burada beklemek hata almamak için gerekli
+driver.execute_script("window.open('');")
+driver.switch_to.window(driver.window_handles[1])
+driver.get(OGLE_YEMEK)
+
+yemek_sec()
+driver.switch_to.window(driver.window_handles[2])
+driver.get(AKSAM_YEMEK)
+yemek_sec()
+driver.get(SEPET)
+
+driver.find_element(By.ID, "ContentPlaceHolder1_btnSatinAl").click()
+
+while (True):
+    pass
